cbf23f5948aff713291114eb0c8b2a47ded8f98c
ProfVespertino <ProfVespertino@C4PROFESOR>
2017-03-31 20:31:05 +0200

    Primer Comit

---
+++ Taller/src/gestion/Coche.java
@@ -0,0 +1,37 @@
+package gestion;
+
+/**
+ * 
+ * @author ProfVespertino
+ *Esta clase representa un coche
+ */
+public class Coche {
+	String matricula;
+	int motor;
+	/**
+	 * @return the matricula
+	 */
+	public String getMatricula() {
+		return matricula;
+	}
+	/**
+	 * @param matricula the matricula to set
+	 */
+	public void setMatricula(String matricula) {
+		this.matricula = matricula;
+	}
+	/**
+	 * @return the motor
+	 */
+	public int getMotor() {
+		return motor;
+	}
+	/**
+	 * @param motor the motor to set
+	 */
+	public void setMotor(int motor) {
+		this.motor = motor;
+	}
+	
+
+}
